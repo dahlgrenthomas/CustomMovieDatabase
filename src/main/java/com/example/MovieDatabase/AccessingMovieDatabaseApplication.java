ce60e6924e9d05dd021cd7df33f3be5d6d1bdff9
Thomas <dahlg136@morris.umn.edu>
2023-09-01 01:12:29 -0500

    Fix repo

--- src/main/java/com/example/MovieDatabase/AccessingMovieDatabaseApplication.java
+++ src/main/java/com/example/MovieDatabase/AccessingMovieDatabaseApplication.java
@@ -2,19 +2,15 @@ package com.example.MovieDatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @SpringBootApplication
-public class AccessingMovieDatabaseApplication{
+public class AccessingMovieDatabaseApplication  {
 
 
   public static void main(String[] args) {
     SpringApplication.run(AccessingMovieDatabaseApplication.class, args);
   }
-
-
 
 
   // @Bean
