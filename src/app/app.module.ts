62b09292e29d7f6db3e586f00aef6ffe4188cbcc
Thomas <dahlg136@morris.umn.edu>
2023-08-31 22:55:10 -0500

    Change angular routings so refreshing the page works
    Added "hash" routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieInfoComponent } from './movie-info/movie-info.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 
 @NgModule({
@@ -37,7 +38,7 @@ import { MovieInfoComponent } from './movie-info/movie-info.component';
     MatButtonModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
