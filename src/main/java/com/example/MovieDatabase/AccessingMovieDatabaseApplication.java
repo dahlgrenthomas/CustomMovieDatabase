1dc28d113727ab2f11d65dff957e91a059967856
Thomas <dahlg136@morris.umn.edu>
2023-08-31 23:48:18 -0500

    Change angular's build path to go to spring boot's static folder

--- src/main/java/com/example/MovieDatabase/AccessingMovieDatabaseApplication.java
+++ src/main/java/com/example/MovieDatabase/AccessingMovieDatabaseApplication.java
@@ -2,15 +2,19 @@ package com.example.MovieDatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @SpringBootApplication
-public class AccessingMovieDatabaseApplication  {
+public class AccessingMovieDatabaseApplication{
 
 
   public static void main(String[] args) {
     SpringApplication.run(AccessingMovieDatabaseApplication.class, args);
   }
+
+
 
 
   // @Bean
