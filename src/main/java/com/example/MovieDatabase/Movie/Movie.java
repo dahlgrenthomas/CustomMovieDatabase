89a07bb0160f41312016fad9dd220491ae3035dd
Thomas <dahlg136@morris.umn.edu>
2023-09-03 03:12:49 -0500

    Add basic integration test

--- src/main/java/com/example/MovieDatabase/Movie/Movie.java
+++ src/main/java/com/example/MovieDatabase/Movie/Movie.java
@@ -41,6 +41,9 @@ public class Movie {
   public String getOverview() {
     return overview;
   }
+  public void setOverview(String overview) {
+    this.overview = overview;
+  }
 
   public void setPoster(String poster) {
     this.poster = poster;
---
+++ src/test/java/com/example/MovieDatabase/Movie/MovieControllerIntTest.java
@@ -0,0 +1,73 @@
+package com.example.MovieDatabase.Movie;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class MovieControllerIntTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testAddNewMovie() throws Exception{
+        Movie movie = new Movie();
+        movie.setId(5);
+        movie.setTitle("movie");
+        movie.setPoster("poster.link");
+        movie.setOverview("Fun movie");
+        
+        // ResultActions response = mockMvc.perform(post("/api/employees")
+        //         .contentType(MediaType.APPLICATION_JSON)
+        //         .content(objectMapper.writeValueAsString(movie)));
+
+        RequestBuilder request = MockMvcRequestBuilders.post("/api/employees")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(movie));
+        MvcResult result = mockMvc.perform(request).andReturn();
+        assertEquals(movie, result);
+
+
+
+    }
+
+    @Test
+    void testGetAllMovies() {
+
+    }
+
+    @Test
+    void testGetMovie() {
+
+    }
+
+    @Test
+    void testGetMoviesSearch() {
+
+    }
+
+    @Test
+    void testGetRandomMovies() {
+
+    }
+}
