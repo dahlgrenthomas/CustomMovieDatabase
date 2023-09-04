f14cc64871474a108a904dc3b0a79e9f25a6442b
Thomas <dahlg136@morris.umn.edu>
2023-09-04 04:01:06 -0500

    Add 2 working tests for the movie controller

--- src/test/java/com/example/MovieDatabase/Movie/MovieControllerIntTest.java
+++ src/test/java/com/example/MovieDatabase/Movie/MovieControllerIntTest.java
@@ -1,18 +1,31 @@
 package com.example.MovieDatabase.Movie;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -29,45 +42,58 @@ public class MovieControllerIntTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @BeforeEach
+    void setup(){
+        movieRepository.deleteAll();
+    }
+
     @Test
-    void testAddNewMovie() throws Exception{
+    void testGetAllMovies() throws Exception {
+        List<Movie> listOfMovies = new ArrayList<>();
         Movie movie = new Movie();
-        movie.setId(5);
+        movie.setId(1);
         movie.setTitle("movie");
         movie.setPoster("poster.link");
         movie.setOverview("Fun movie");
-        
-        // ResultActions response = mockMvc.perform(post("/api/employees")
-        //         .contentType(MediaType.APPLICATION_JSON)
-        //         .content(objectMapper.writeValueAsString(movie)));
 
-        RequestBuilder request = MockMvcRequestBuilders.post("/api/employees")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(movie));
-        MvcResult result = mockMvc.perform(request).andReturn();
-        assertEquals(movie.toString(), result.toString());
+        Movie movie2 = new Movie();
+        movie2.setId(2);
+        movie2.setTitle("movie2");
+        movie2.setPoster("poster.link2");
+        movie2.setOverview("Fun movie2");
 
+        listOfMovies.add(movie);
+        listOfMovies.add(movie2);
 
+        movieRepository.saveAll(listOfMovies);
 
-    }
+        ResultActions response = mockMvc.perform(get("/movies/all"));
 
-    @Test
-    void testGetAllMovies() {
-
-    }
-
-    @Test
-    void testGetMovie() {
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()",
+                        is(listOfMovies.size())));
 
     }
 
     @Test
-    void testGetMoviesSearch() {
+    void testGetMovie() throws Exception {
+        Movie movie = new Movie();
+        movie.setId(1);
+        movie.setTitle("movie");
+        movie.setPoster("poster.link");
+        movie.setOverview("Fun movie");
+        movieRepository.save(movie);
+
+        ResultActions response = mockMvc.perform(get("/movies/{id}", movie.getId()));
+
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.title", is(movie.getTitle())))
+                .andExpect(jsonPath("$.poster", is(movie.getPoster())))
+                .andExpect(jsonPath("$.overview", is(movie.getOverview())));
 
     }
 
-    @Test
-    void testGetRandomMovies() {
 
-    }
 }
